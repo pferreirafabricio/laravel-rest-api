1ec6d926f4d632a370a2e323c53984c3f98cbd9c
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2020-05-10 21:51:04 -0300

    Created Product migration, model, factory and seeder

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use Faker\Generator as Faker;
+
+$factory->define(\App\Product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'price' => $faker->randomFloat(2, 0, 8),
+        'description' => $faker->text 
+    ];
+});
---
+++ database/migrations/2020_05_10_233214_create_table_products.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableProducts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->float('price');
+            $table->text('description');
+            $table->timestamps(); //created_at and update_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
