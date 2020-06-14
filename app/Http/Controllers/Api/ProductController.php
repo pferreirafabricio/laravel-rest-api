62f8c8fb2a9cf4af0ee264a80f1d304823f7296f
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2020-06-14 18:52:30 -0300

    feat(api): add show specif product
    Add a new route for show a specif product using ID

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -6,9 +6,23 @@ use App\Product;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class ProductController extends Controller 
+class ProductController extends Controller
 {
-    public function index() {
-        return Product::all();
+    private $product;
+
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
+
+    public function index()
+    {
+        return response()->json($this->product->paginate(5));
+    }
+
+    public function show(Product $id)
+    {
+        $data = ['data' => $id];
+        return response()->json($data);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::namespace('API')->name('api.')->group(function() {
-    Route::prefix('products')->group(function() {
-        Route::get('/','ProductController@index')->name('products');
+Route::namespace('API')->name('api.')->group(function () {
+    Route::prefix('products')->group(function () {
+
+        Route::get('/', 'ProductController@index')->name('index_products');
+        Route::get('/{id}', 'ProductController@show')->name('single_product');
     });
 });
